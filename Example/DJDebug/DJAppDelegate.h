da067c772c3cca7b5b487082ee8e4907bbe9dd82
Deju Liu <cheneylew@163.com>
2016-12-02 10:21:26 +0800

    no message

---
+++ Example/DJDebug/DJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DJAppDelegate.h
+//  DJDebug
+//
+//  Created by Deju Liu on 12/02/2016.
+//  Copyright (c) 2016 Deju Liu. All rights reserved.
+//
+
+@import UIKit;
+
+@interface DJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/DJDebug/DJViewController.h
@@ -0,0 +1,13 @@
+//
+//  DJViewController.h
+//  DJDebug
+//
+//  Created by Deju Liu on 12/02/2016.
+//  Copyright (c) 2016 Deju Liu. All rights reserved.
+//
+
+@import UIKit;
+
+@interface DJViewController : UIViewController
+
+@end
